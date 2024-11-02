044455cdd03fd365ca653a6dbc319b22973635bc
R-Harikrishna9 <harikrishnaravvarapu9@gmail.com>
2024-11-02 12:20:05 +0530

    Add key_manager.py

---
+++ encryption_service/key_manager.py
@@ -0,0 +1,21 @@
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+import os
+
+class KeyManager:
+    @staticmethod
+    def generate_key(password: str, salt: bytes) -> bytes:
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=100000,
+            backend=default_backend()
+        )
+        return kdf.derive(password.encode())
+
+    @staticmethod
+    def generate_salt() -> bytes:
+        return os.urandom(16)
+            
